63ad0d34d5883a94aebc92f4e9b332ba0b7750c6
m4x809 <github@j-pet.de>
2025-05-31 17:27:01 +0200

    Update manifest and script to include support for WowTV; refactor code for improved readability and maintainability.

--- code/script.js
+++ code/script.js
@@ -1,87 +1,83 @@
 (() => {
-  // console.log("%cT.E.T.W. script loaded", "color: green; font-size: 20px;");
-  let interval;
+	// console.log("%cT.E.T.W. script loaded", "color: green; font-size: 20px;");
+	let interval;
 
-  chrome.storage.sync.get(["extensionEnabled"], (result) => {
-    if (result.extensionEnabled) {
-      startSkipIntro();
-    }
-  });
+	chrome.storage.sync.get(["extensionEnabled"], (result) => {
+		if (result.extensionEnabled) {
+			startSkipIntro();
+		}
+	});
 
-  chrome.storage.onChanged.addListener((changes, namespace) => {
-    // console.log(changes.extensionEnabled);
-    if (changes.extensionEnabled) {
-      if (changes.extensionEnabled.newValue) {
-        startSkipIntro();
-      } else {
-        stopSkipIntro();
-      }
-    }
-  });
+	chrome.storage.onChanged.addListener((changes, namespace) => {
+		// console.log(changes.extensionEnabled);
+		if (changes.extensionEnabled) {
+			if (changes.extensionEnabled.newValue) {
+				startSkipIntro();
+			} else {
+				stopSkipIntro();
+			}
+		}
+	});
 
-  async function waitMS(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
+	async function waitMS(ms) {
+		return new Promise((resolve) => setTimeout(resolve, ms));
+	}
 
-  function startSkipIntro() {
-    if (interval) return;
+	function debugButtons() {
+		const labeledButtons = document.querySelectorAll("button.hasLabel");
+		console.log("Buttons with hasLabel class:", Array.from(labeledButtons));
+	}
 
-    // console.log(
-    //   "%cStarting skip intro/ads interval...",
-    //   "color: red; font-size: 20px;"
-    // );
-    interval = setInterval(async () => {
-      const netflixSkipButton = document.querySelector(
-        'button[data-uia="player-skip-intro"]'
-      );
-      const netflixNextEpisodeButton = document.querySelector(
-        'button[data-uia="next-episode-seamless-button"]'
-      );
-      const netflixSkipRecapButton = document.querySelector(
-        'button[data-uia="player-skip-recap"]'
-      );
-      const primeSkipButton = document.querySelector(
-        ".atvwebplayersdk-skipelement-button"
-      );
-      const primeNextEpisodeButton = document.querySelector(
-        ".atvwebplayersdk-nextupcard-button"
-      );
-      const disneySkipEpisodeButton = document.querySelector(
-        '.skip__button'
-      );
-      const disneyNextEpisodeButton = document.querySelector(
-        '[aria-label^="NEXT EPISODE IN"]'
-      );
+	function startSkipIntro() {
+		if (interval) return;
 
-      const buttons = [
-        netflixSkipButton,
-        netflixNextEpisodeButton,
-        netflixSkipRecapButton,
-        primeSkipButton,
-        primeNextEpisodeButton,
-        disneySkipEpisodeButton,
-        disneyNextEpisodeButton,
-      ];
+		// console.log(
+		//   "%cStarting skip intro/ads interval...",
+		//   "color: red; font-size: 20px;"
+		// );
+		interval = setInterval(async () => {
+			debugButtons();
+			const netflixSkipButton = document.querySelector('button[data-uia="player-skip-intro"]');
+			const netflixNextEpisodeButton = document.querySelector('button[data-uia="next-episode-seamless-button"]');
+			const netflixNextEpisodeButton2 = document.querySelector('button[data-uia="next-episode-seamless-button-draining"]');
+			const netflixSkipRecapButton = document.querySelector('button[data-uia="player-skip-recap"]');
+			const primeSkipButton = document.querySelector(".atvwebplayersdk-skipelement-button");
+			const primeNextEpisodeButton = document.querySelector(".atvwebplayersdk-nextupcard-button");
+			const disneySkipEpisodeButton = document.querySelector(".skip__button");
+			const disneyNextEpisodeButton = document.querySelector('[aria-label^="NEXT EPISODE IN"]');
+			const wowNextEpisodeButton = document.querySelector("a.playback-binge__card");
 
-      const buttonToClick = buttons.find((button) => button);
+			const buttons = [
+				netflixSkipButton,
+				netflixNextEpisodeButton,
+				netflixNextEpisodeButton2,
+				netflixSkipRecapButton,
+				primeSkipButton,
+				primeNextEpisodeButton,
+				disneySkipEpisodeButton,
+				disneyNextEpisodeButton,
+				wowNextEpisodeButton,
+			];
 
-      if (buttonToClick) {
-        console.log("Button found:", buttonToClick);
-        buttonToClick.click();
-        stopSkipIntro();
-        await waitMS(5000);
-        startSkipIntro();
-      } else {
-        console.log("No actionable buttons found.");
-      }
-    }, 1000);
-  }
+			const buttonToClick = buttons.find((button) => button);
 
-  function stopSkipIntro() {
-    clearInterval(interval);
-    interval = null;
-  }
+			if (buttonToClick) {
+				console.log("Button found:", buttonToClick);
+				buttonToClick.click();
+				stopSkipIntro();
+				await waitMS(1000);
+				startSkipIntro();
+			} else {
+				console.log("No actionable buttons found.");
+			}
+		}, 1000);
+	}
 
-  // Make stopSkipIntro available globally for the popup
-  window.stopSkipIntro = stopSkipIntro;
+	function stopSkipIntro() {
+		clearInterval(interval);
+		interval = null;
+	}
+
+	// Make stopSkipIntro available globally for the popup
+	window.stopSkipIntro = stopSkipIntro;
 })();
